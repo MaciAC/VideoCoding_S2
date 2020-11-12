870ec9760f85d168128ec002bd85330031846de5
MaciAC <macia.ac@gmail.com>
2020-11-12 16:55:32 +0100

    gitignora changes

--- Video_editor.py
+++ Video_editor.py
@@ -1,8 +1,26 @@
+import os
 
 menu = open("menu.txt", "r").read()
 exit = False
+change_vid = True
 
-while exit == False:
+while not exit:
+
+    if change_vid:
+        filename = input("\nQuin video vols editar?\n")
+
     option = int(input(menu))
+
     if option == 0:
         exit = True
+        continue
+
+    if option == 1:
+        start = input("\nSelect start time in format: hh:mm:ss \n")
+        time = int(input("\nHow many seconds? \n"))
+
+        os.system("ffmpeg -ss {} -i {} -t {} -c copy {}".format(start, filename, time, 'cut' + filename))
+
+    if option == 2:
+
+        os.system('ffmpeg -i {} -vf "split=2[a][b],[b]histogram,format=yuva444p[hh],[a][hh]overlay" {}'.format( filename, 'hist' + filename))
