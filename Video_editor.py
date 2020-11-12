633085fbf7dcccd0761acc9b50cb87bba891b86f
MaciAC <macia.ac@gmail.com>
2020-11-12 18:35:00 +0100

    Post lab2 session, already finished. Needs README

--- Video_editor.py
+++ Video_editor.py
@@ -16,11 +16,26 @@ while not exit:
         continue
 
     if option == 1:
-        start = input("\nSelect start time in format: hh:mm:ss \n")
-        time = int(input("\nHow many seconds? \n"))
+        start = input("\nEspecifica l'inici en el format: hh:mm:ss \n")
+        time = input("\nQuants segons vols? \n")
+        out = "cut_{}seconds_{}".format(time, filename)
+        os.system("ffmpeg -i {} -ss {} -t {} -c copy {}".format(filename, start, time, out))
 
-        os.system("ffmpeg -ss {} -i {} -t {} -c copy {}".format(start, filename, time, 'cut' + filename))
 
     if option == 2:
+        out = 'hist_' + filename
+        os.system('ffmpeg -i {} -vf "split=2[a][b],[b]histogram,format=yuva444p[hh],[a][hh]overlay" {}'.format( filename, out))
 
-        os.system('ffmpeg -i {} -vf "split=2[a][b],[b]histogram,format=yuva444p[hh],[a][hh]overlay" {}'.format( filename, 'hist' + filename))
+
+    if option == 3:
+        out = "resized_" + filename
+        sizes = ["1280:720", "720:480", "360:240", "160:120"]
+        idx = int(input("\nA quina resolució el vols canviar?\n 1: 720p\n 2: 480p\n 3: 360x240\n 4: 160x120 \n")) - 1
+        os.system("ffmpeg -i {} -vf scale={},setsar=1:1 {}".format(filename, sizes[idx], out))
+
+
+    if option == 4:
+        out = "monoAAC_" + filename
+        os.system("ffmpeg -i {} -c:v copy -ac 1 -c:a aac {}".format(filename, out))
+
+    print("\nGuardat com a " + out)
